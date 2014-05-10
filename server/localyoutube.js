978156fcb510b06f61bd23eebe9b552499013746
samuelharden <gauravarora.daiict@gmail.com>
2014-05-10 15:34:14 +0000

    Added server file

---
+++ server/localyoutube.js
@@ -0,0 +1,130 @@
+/*
+ * Inspired by: http://stackoverflow.com/questions/4360060/video-streaming-with-html-5-via-node-js
+ */
+ 
+var https = require('https'),
+    http = require('http'),
+    fs = require('fs'),
+    util = require('util'),
+    tls = require('tls'),
+    ytdl = require('ytdl');
+
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('key-cert.pem')
+};
+
+http.createServer(function (req, res) {
+var path = "."+req.url;
+var id = req.url.split(".")[0].split("/")[1];
+var ytdlurl = "https://www.youtube.com/watch?v="+id
+    console.log(path);
+    console.log(ytdlurl);
+  try {
+      stat = fs.statSync(path);
+      var total = stat.size;
+      if (total != 0) {
+        res.writeHead(200);      
+        res.end();
+      } 
+      else {
+  //      console.log(req);
+        downloadFile(req,path,ytdlurl);
+        res.writeHead(500);
+        res.end();
+      }
+    
+    }
+    catch(err) {
+    //    console.log(req);
+        downloadFile(req,path,ytdlurl);
+        res.writeHead(500);
+        res.end();
+    }
+}).listen(8001, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8001/');
+
+var cleartextStream = https.createServer(options,function (req, res) {
+//  var path = 'video.mp4';
+  var path = "."+req.url;
+  console.log(path);
+  var stat;
+  try {
+      stat = fs.statSync(path);
+  var total = stat.size;
+  if (req.headers['range']) {
+    nowsend(req,res,path,total);
+  } else {
+    console.log('ALL: ' + total);
+    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+    fs.createReadStream(path).pipe(res);
+  }
+    }
+    catch(err) {
+        console.log(err);
+        console.log('Opened FIle now' );
+        downloadFile(req,path);
+        setTimeout(letsexecute(req,res,path,total,end),30);
+  }
+}).listen(8000, '127.0.0.1');
+
+console.log('Server running at https://127.0.0.1:8000/');
+
+downloadFile = function(req,path,ytdlurl) {
+    try {
+        if (ytdlurl) {
+            ytdl(ytdlurl,{ filter: function(format) { return format.container === 'mp4'; } }).pipe(fs.createWriteStream(path));
+        }
+        else {
+            ytdl(req.headers['referer'],{ filter: function(format) { return format.container === 'mp4'; } }).pipe(fs.createWriteStream(path));
+        }
+    }
+    catch(err ) {
+        console.log("problem downloading the video, lets not stop the script: "+ err);
+    }
+}   
+
+letsexecute = function(req,res,path,total,end) {
+    stat = fs.statSync(path);
+    var end = stat.size;
+    console.log("now getting the end: "+end);
+    if (req.headers['range']) {
+        var total = stat.size;
+            nowsend(req,res,path,total,end);    
+        }
+}
+
+nowsend = function (req,res,path,total,end1){
+    var range = req.headers.range;
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+ 
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : total-1;
+    var chunksize = (end-start)+1;
+    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+    console.log('Partial: ' + partialend +' total '+total+' end1 '+ end1);
+    var orignaltotal = total
+    if (end1) {
+        end = partialend;
+        total = partialend;
+    }
+    try {
+        if (orignaltotal == 0)  {
+            throw new Error("No content to let it go to youtube");
+        }
+        res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+        var file = fs.createReadStream(path, {start: start, end: end});
+        file.pipe(res);
+//        fs.createReadStream(path).pipe(res);
+    }
+    catch (err) {
+        console.log("problem sending downloading file"+ err)
+        res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+        fs.createReadStream(path).pipe(res);
+        res.statusCode = 500;
+        res.end();
+    }
+}
