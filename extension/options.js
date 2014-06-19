969943b04da0fc4552f001ac69878cccbf1558f7
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-06-19 15:13:13 +0530

    Option page html done inital version

--- extension/options.js
+++ extension/options.js
@@ -6,6 +6,13 @@ function save_options() {
         setTimeout(function(){document.getElementById('status').textContent = '' ;},1000);
     });
 }
+function save_serveraddr() {
+    var serveriptext = document.getElementById('serverip').value;
+    chrome.storage.sync.set({'serverip': serveriptext}, function() {
+        document.getElementById('serveripstatus').textContent = "Server ip Saved";
+        setTimeout(function(){document.getElementById('serveripstatus').textContent = '' ;},1000);
+    });
+}
 
 function restore_options() {
 chrome.storage.sync.get({
@@ -13,10 +20,24 @@ chrome.storage.sync.get({
   },function(item) {
     document.getElementById('list').checked = item.list;
 });
+chrome.storage.sync.get({
+    serverip: "127.0.0.1",
+  },function(item) {
+    document.getElementById('serverip').value = item.serverip;
+});
 
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+$(document).ready(function () {
 
+    $("#tabs").tabs();
+    document.getElementById('save').addEventListener('click',
+    save_options);
+    document.getElementById('saveserverip').addEventListener('click',
+    save_serveraddr);
+
+});
+
+
+$("#tabs").tabs();
+document.addEventListener('DOMContentLoaded', restore_options);
